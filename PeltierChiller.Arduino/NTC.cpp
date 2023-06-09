e59d7d2803e6242edf8f4c0c1fa981c7e629b895
s1kven <s1kvengit@gmail.com>
2023-06-10 01:01:48 +0300

    Added NTC sensor.

---
+++ PeltierChiller.Arduino/NTC.cpp
@@ -0,0 +1,30 @@
+#include "NTC.h"
+
+Models::TemperatureSensors::NTC::NTC(uint8_t sensorAddress, uint32_t resistance, uint16_t bCoefficient,
+	uint32_t resistanceNTC, Models::Enums::TemperatureSensorTarget sensorTarget, uint8_t baseNTCTemp) :
+	Models::TemperatureSensors::BaseSensor(sensorTarget, Models::Enums::TemperatureSensorType::NTC)
+{
+	_sensorAddress = sensorAddress;
+	_resistance = resistance;
+	_bCoefficient = bCoefficient;
+	_resistanceNTC = resistanceNTC;
+	_baseNTCTemp = baseNTCTemp;
+}
+
+float Models::TemperatureSensors::NTC::getTemperature()
+{
+	return _temperature;
+}
+
+void Models::TemperatureSensors::NTC::sensorRequest()
+{
+	float value = analogRead(_sensorAddress);
+	value = ((float) _resistanceNTC / _resistance) / (1023.0f / value - 1.0);
+	value = (log(value) / _bCoefficient) + 1.0 / (_baseNTCTemp + _zeroCbyK);
+	_temperature = (1.0 / value - _zeroCbyK);
+}
+
+uint8_t Models::TemperatureSensors::NTC::getSensorAddress()
+{
+	return uint8_t();
+}
---
+++ PeltierChiller.Arduino/NTC.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "BaseSensor.h"
+
+namespace Models::TemperatureSensors
+{
+
+	class NTC :
+		public Models::TemperatureSensors::BaseSensor
+	{
+	private:
+
+		const float _zeroCbyK = 273.15f;
+
+		uint8_t _sensorAddress;
+		uint32_t _resistance;
+		uint16_t _bCoefficient; 
+		uint32_t _resistanceNTC;
+		uint8_t _baseNTCTemp;
+
+	protected:
+
+
+	public:
+
+		NTC(uint8_t sensorAddress, uint32_t resistance, uint16_t bCoefficient, uint32_t resistanceNTC,
+			Models::Enums::TemperatureSensorTarget sensorTarget, uint8_t baseNTCTemp = 25);
+		float getTemperature();
+		void sensorRequest() override;
+		uint8_t getSensorAddress();
+	};
+}
