b71aff48b7dad6ff1ae3419b8be58b5e8e4945c7
DESKTOP-M6RV24C\vzakh <s1kvengit@gmail.com>
2023-06-10 00:03:52 +0300

    Added DS18B20 sensor.

---
+++ PeltierChiller.Arduino/DS18B20.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <stdint.h>
+#include "BaseSensor.h"
+
+namespace Models::TemperatureSensors
+{
+
+	class DS18B20 :
+		public Models::TemperatureSensors::BaseSensor
+	{
+	private:
+
+		DallasTemperature* _sensor;
+		DeviceAddress* _sensorAddress;
+
+	protected:
+
+
+	public:
+
+		DS18B20(DeviceAddress* sensorAddress, Models::Enums::TemperatureSensorTarget _sensorTarget);
+		void init(DallasTemperature* sensor, uint8_t _temperaturePrecision) ;
+		float getTemperature();
+		void sensorRequest() override;
+		DeviceAddress* getSensorAddress();
+	};
+}
+
