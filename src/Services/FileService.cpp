0e1891a1c9e1899f36001ecfd31f0dd0fb3b2db8
s1kven <zaxaro.vladislav@gmail.com>
2025-02-18 22:33:14 +0300

    feat: FileService redesigned from SD to SdFat.

--- src/Services/FileService.cpp
+++ src/Services/FileService.cpp
@@ -1,27 +1,39 @@
 #include "FileService.h"
 
-void Services::FileService::init(uint8_t cs)
+bool Services::FileService::createDirectory(const char *directory)
 {
-	SD.begin(cs);
+	if (_sd.exists(directory) || (!_sd.exists(directory) && _sd.mkdir(directory))) 
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Services::FileService::init(uint8_t cs)
+{
+	return _sd.begin(cs, SD_SCK_MHZ(16));
 }
 
 String Services::FileService::readFile(const char* path)
 {
 	String content = "";
-	File file = SD.open(path);
-	if (file) 
+    SdFile file;
+	if (file.open(path, O_READ)) 
 	{
-		content = file.readString();
-		file.close();
+		while (file.available()) 
+        {
+            content += (char)file.read();
+        }
+        file.close();
 	}
 	return content;
 }
 
-uint32_t Services::FileService::appendFile(const char* path, String content)
+uint32_t Services::FileService::appendFile(const char *fileName, String content)
 {
 	uint32_t bytesWrite = 0;
-	File file = SD.open(path, FILE_APPEND);
-	if (file)
+	SdFile file;
+	if (file.open(fileName, O_RDWR | O_AT_END))
 	{
 		bytesWrite = file.println(content);
 		file.close();
@@ -29,14 +41,72 @@ uint32_t Services::FileService::appendFile(const char* path, String content)
 	return bytesWrite;
 }
 
-uint32_t Services::FileService::writeFile(const char* path, String content)
+uint32_t Services::FileService::appendFile(const char *path, const char *fileName, String content)
 {
-	uint32_t bytesWrite = 0;
-	File file = SD.open(path, FILE_WRITE);
-	if (file)
+	if(createDirectory(path) && _sd.open(path, O_DIRECTORY))
 	{
-		bytesWrite = file.println(content);
-		file.close();
+		return appendFile((String(path) + String(fileName)).c_str(), content);
 	}
-	return bytesWrite;
+	return 0;
+}
+
+uint32_t Services::FileService::appendFile(const char *path, const char *fileName, std::queue<String> &content)
+{
+	if(createDirectory(path) && _sd.open(path, O_DIRECTORY))
+	{
+		uint32_t bytesWrite = 0;
+		SdFile file;
+		if (file.open((String(path) + String(fileName)).c_str(), O_RDWR | O_AT_END))
+		{
+			bytesWrite = 0;
+			while(!content.empty())
+			{
+				bytesWrite += file.println(content.front());
+				content.pop();
+			}
+			file.close();
+		}
+		return bytesWrite;
+	}
+	return 0;
+}
+
+uint32_t Services::FileService::writeFile(const char *fileName, String content)
+{
+    uint32_t writeBytes = 0;
+    FsFile file = _sd.open(fileName, O_RDWR | O_CREAT | O_TRUNC);
+    if (file) 
+    {
+        writeBytes = file.println(content);
+        file.close();
+    }
+    return writeBytes;
+}
+
+uint32_t Services::FileService::writeFile(const char *path, const char *fileName, String content)
+{
+	if(createDirectory(path) && _sd.open(path, O_DIRECTORY))
+	{
+		return writeFile((String(path) + String(fileName)).c_str(), content);
+	}
+	return 0;
+}
+
+bool Services::FileService::createFile(const char *fileName)
+{
+	Serial.println(fileName);
+    FsFile file;
+    if(file.open(fileName, O_RDWR | O_CREAT | O_AT_END | O_TRUNC))
+    {
+        file.close();
+		Serial.println("created");
+        return true;
+    }
+	Serial.println("!created");
+    return false;
+}
+
+bool Services::FileService::createFile(const char *path, const char *fileName)
+{
+    return createDirectory(path) && _sd.open(path, O_DIRECTORY) && createFile((String(path) + String(fileName)).c_str());
 }
--- src/Services/FileService.h
+++ src/Services/FileService.h
@@ -3,23 +3,34 @@
 #define _FileService_ 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "arduino.h"
+#include <queue>
 #else
 #include "WProgram.h"
 #endif
 
-#include <FS.h>
-#include <SD.h>
+#include <SPI.h>
+#include <SdFat.h>
 
 namespace Services
 {
 	class FileService
 	{
+    private:
+        SdFat _sd;
+
+		bool createDirectory(const char* directory);
+
 	public:
-		void init(uint8_t cs);
+		bool init(uint8_t cs);
 
 		String readFile(const char* path);
-		uint32_t appendFile(const char* path, String content);
-		uint32_t writeFile(const char* path, String content);
+		uint32_t appendFile(const char* fileName, String content);
+		uint32_t appendFile(const char* path, const char* fileName, String content);
+		uint32_t appendFile(const char* path, const char* fileName, std::queue<String>& content);
+		uint32_t writeFile(const char* fileName, String content);
+		uint32_t writeFile(const char* path, const char* fileName, String content);
+		bool createFile(const char* fileName);
+		bool createFile(const char* path, const char* fileName);
 	};
 }
 #endif
